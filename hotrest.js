84b8cd37fc10c518a3003f56509a3881aadd6eaf
timhayes00 <timhayes00@gmail.com>
2018-03-30 10:55:46 -0400

    added

--- hotrest.js
+++ hotrest.js
@@ -8,6 +8,39 @@ var app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+var tables = [];
+var waitList = [];
+var table = {
+    "name": "Tim",
+    "phone": "867-5309",
+    "email": "tim@gmail.com",
+    "id": "Keyboard Cat"
+}
+
+tables[0] = table;
+console.log (tables);
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+  });
+  
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+  
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+  
+  app.get("/api/tables", function(req, res) {
+    
+  });
+  
+  app.get("/api/waitlist", function(req, res) {
+    
+  });
+
+
 
 app.listen(PORT, function() {
     console.log("Server listening on http://localhost:" + PORT);
