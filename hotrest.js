37aa58018403e98acf39a523846ec27e9760d7bc
timhayes00 <timhayes00@gmail.com>
2018-03-30 11:15:27 -0400

    hmmmmm

--- hotrest.js
+++ hotrest.js
@@ -33,7 +33,7 @@ app.get("/", function(req, res) {
   });
   
   app.get("/api/tables", function(req, res) {
-    
+    return res.json(tables);
   });
   
   app.get("/api/waitlist", function(req, res) {
